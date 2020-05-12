7fc7fc5d5a22bdad18733d684e508bcb82e9d9a6
YarinBS <yarin10121@gmail.com>
2020-05-12 21:15:35 +0300

    flights.py updated

---
+++ flights.py
@@ -0,0 +1,15 @@
+class Flights:
+    def __init__(self, data):
+        self.data = data
+
+    def filter_by_airport_names(self, airports, letters):
+        pass
+
+    def print_details(self, features, statistic_functions):
+        pass
+
+    def compute_empty_seats(self):
+        pass
+
+    def count_bad_flights(self, num):
+        pass
