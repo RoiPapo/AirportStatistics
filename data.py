2511ce1d38ce1d703451003c4336e780d6235216
Roi <thedarkhermit@gmail.com>
2020-05-12 21:32:01 +0300

    first commint of adding csvs

---
+++ data.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+
+class Data:
+    data = dict()
+
+    def __init__(self, path):
+        df = pd.read_csv(path)
+        data = df.to_dict(orient="list")
+
+    def select_features(self, features):
+        self.data = {k: self.data[k] for k in features.split()}  # construct dict only with the features we have
--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("WOOOOOW")
+import sys
+
+
+def main(argv):
+    path = argv[1]
+    features = argv[2]
+    features_p1 = ['Origin_airport', 'Destination_airport', 'Flights', 'Distance', 'Seats', 'Passengers']
+    if __name__ == "__main__":
+        main(sys.argv)
